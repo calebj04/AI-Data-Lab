45900752d04902f4d464369d6520d1ff0f338e43
Caleb Jennings <calebbirtch.j@gmail.com>
2025-10-21 18:22:41 -0400

    init

---
+++ player_season_stats.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Read the data
+df = pd.read_csv('data/nfl_kick_attempts.csv')
+
+# Select relevant columns
+kicker_seasons = df[["kicker_player_id", "kicker_player_name", "season"]]
+
+# Count number of kicks per player per season
+kicker_season_counts = (
+    kicker_seasons
+    .groupby(["kicker_player_id", "kicker_player_name", "season"])
+    .size()
+    .reset_index(name="kicks_in_season")
+)
+
+# Sort by player and season
+kicker_season_counts.sort_values(by=["kicker_player_id", "season"], inplace=True)
+
+# Save to CSV
+kicker_season_counts.to_csv('data/kicker_seasons.csv', index=False)
