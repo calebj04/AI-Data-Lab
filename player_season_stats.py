b609c367e3bc8bc9344c00c2d5cf100c2efff36e
LoganJC <loganchasen06@gmail.com>
2025-10-28 18:47:13 -0400

    test

--- player_season_stats.py
+++ player_season_stats.py
@@ -8,6 +8,7 @@ df = pd.read_csv('data/nfl_kick_attempts.csv')
 # Select relevant columns
 kicker_seasons = df[["kicker_player_id", "kicker_player_name", "season"]]
 
+#git test
 # Count number of kicks per player per season
 kicker_season_counts = (
     kicker_seasons
